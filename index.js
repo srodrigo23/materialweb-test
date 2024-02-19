8573caeb6032a655d12a1e664a7566ff0c7f4735
rodres101 <rodrigosergio93@gmail.com>
2024-02-19 12:17:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/button/elevated-button.js'
+
+
+import '@material/web/chips/chip-set'
+import '@material/web/chips/chip-set'
+import '@material/web/chips/assist-chip'
+import '@material/web/chips/filter-chip'
+import '@material/web/chips/input-chip'
+import '@material/web/chips/suggestion-chip'
